6efbab0f13807f7ff4e845bdeac5ca7a5db8ed36
Matthias Mauritz <Matthias.mauritz1@gmail.com>
2021-03-24 18:27:48 +0100

    added more info

--- NewShoppingCart/Petstore/src/app/products.ts
+++ NewShoppingCart/Petstore/src/app/products.ts
@@ -1,17 +1,38 @@
 export const products = [
     {
-        name: 'Dog food', 
+        name: 'Flies', 
         price: 7.99,
-        description: 'A large bag of premium dog food'
+        description: 'A large bag of premium Flies',
+        img:
       },
       {
         name: 'Lizzard',
         price: 10,
-        description: 'Cool lizzard to cuddle with'
+        description: 'Cool lizzard to cuddle with',
+        img:
       },
       {
         name: 'Axolotl',
         price: 35,
-        description: 'Cool animal you can chop his paw off and it grows back'
+        description: 'Cool animal you can chop his paw off and it grows back',
+        img:
+      },
+      {
+        name: 'Chameleon',
+        price: 29.99,
+        description: 'A pet that will blend into your background and you might lose it',
+        img:
+      },
+      {
+        name: 'Bearded Dragon',
+        price: 199.99,
+        description: 'A dragon with a beard',
+        img:
+      },
+      {
+        name: 'Merauke Blue-Tongue Skink',
+        price: 79,
+        description: 'It is a Merauke Blue-tongue Skink',
+        img:
       }
 ]
