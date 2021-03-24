16f12b83beffe58ad3db392d495b9635cb1aac02
Matthias Mauritz <Matthias.mauritz1@gmail.com>
2021-03-24 14:11:45 +0100

    shopping Cart in angular! :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component'; // does the order matter?
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [{
+  path:"", component: ProductListComponent
+},{
+  path: "products/:productId", component: ProductDetailsComponent
+},
+{
+  path: "cart", component: CartComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service'; //does the order of this matter?
+import { products } from '../products';
+
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: any;
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
+
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+    });
+  }
+
+}
