fabf22dd508b96441e8949ad0247ba71449d2a5e
Evelyn Gistelinck <evelyngistelinck@hotmail.com>
2021-03-24 15:34:25 +0100

    start making petstore

---
+++ NewShoppingCart/Petstore/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ProductListComponent,
+    ProductDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ NewShoppingCart/Petstore/src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'Dog food', 
+        price: 7.99,
+        description: 'A large bag of premium dog food'
+      },
+      {
+        name: 'Lizzard',
+        price: 10,
+        description: 'Cool lizzard to cuddle with'
+      },
+      {
+        name: 'Axolotl',
+        price: 35,
+        description: 'Cool animal you can chop his paw off and it grows back'
+      }
+]
